25aedb25baa053defac11f209681121ebe19b6c1
Laura <laura.a.chelaru@gmail.com>
2020-11-16 19:43:54 +0100

    sistemata ricerca attori e pagina di benvenuto

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ $("#cerca").click(function() {
     clearSearch();
     movieTvSearch("movie", inputUtente);
     movieTvSearch("tv", inputUtente);
+    $("#homepage-message").addClass('d-none');
   }
 });
 
@@ -23,6 +24,7 @@ $("#search-input").keyup(
         clearSearch();
         movieTvSearch("movie", inputUtente);
         movieTvSearch("tv", inputUtente);
+        $("#homepage-message").addClass('d-none');
       }
     }
   });
@@ -101,43 +103,37 @@ function notFound(type) {
 // Funzione che prende un array e stampa ogni suo FILM nel template handlebars
 function movieTvPrint(type,objectArray) {
   for(var i=0; i < objectArray.length; i++) {
+
     // Prendiamo il template di handlebars
     var source = $("#element-template").html();
     var template = Handlebars.compile(source);
+
     // Controlliamo se il poster_path è diverso da null
     if(objectArray[i].poster_path != null) {
       var isPosterJS = true;
     } else {
       var isPosterJS = false;
     }
+
+    // Controlliamo se il titolo originale è diverso da quello già mostrato
+    if(objectArray[i].title != objectArray[i].original_title || objectArray[i].name != objectArray[i].original_name) {
+      var hasOgTitle = true;
+    } else {
+      var hasOgTitle = false;
+    }
+
     // Calcoliamo il voto da 1 a 5
     var vote10 = objectArray[i].vote_average;
     var vote5 = convertVote(vote10, 10);
-    // Facciamo una chiamata ajax degli attori prendendo l'id e il type
-    var movieId = (objectArray[i].id).toString();
-    var actorsArray = [];
-    $.ajax({
-      "url": "https://api.themoviedb.org/3/"+type+"/"+movieId+"/credits",
-      "data": {
-        "api_key": "4c51a288148bd58a06eb503205aefc6f",
-      },
-      "method": "GET",
-      "success": function(data) {
-        var risultati = data.cast;
-        actorsArray.push(risultati[0].name);
-      },
-      "error": function(err) {
-        alert("errore!");
-      }
-    });
-    console.log("Actors: " + actorsArray);
+
     // Riempiamo il template di handlebars
     var content = {
+      "movie-id": objectArray[i].id,
       "title": objectArray[i].title || objectArray[i].name,
       "isPosterHTML": isPosterJS,
       "poster-path": objectArray[i].poster_path,
+      "hasOriginalTitle": hasOgTitle,
       "original_title": objectArray[i].original_title || objectArray[i].original_name,
-      "attori5": actorsArray.join(", "),
       "img-flag": savedFlags(objectArray[i].original_language),
       "flag-lang": objectArray[i].original_language,
       "vote_average": starPrint(vote5),
@@ -152,6 +148,9 @@ function movieTvPrint(type,objectArray) {
     }
     // Facciamo sì che la scrollbar della lista sia impostata all'inizio
     $("ul").scrollLeft(0);
+
+    // Ricerchiamo gli attori
+    getActors(type, objectArray[i].id);
   }
 }
 
@@ -174,9 +173,40 @@ $("main").on("click", "section > i", function() {
 });
 
 // Funzione che, preso l'id di un film o serie tv, ne cerca gli attori
-// function getActors(type, movieId) {
-//
-// }
+function getActors(type, id) {
+  var actorsArray = [];
+
+  $.ajax({
+    "url": "https://api.themoviedb.org/3/"+type+"/"+id+"/credits",
+    "data": {
+      "api_key": "4c51a288148bd58a06eb503205aefc6f",
+    },
+    "method": "GET",
+    "success": function(data) {
+      var risultati = data.cast;
+      if(risultati.length > 0) {
+        if(risultati.length < 4) {
+          for(var i = 0; i < risultati.length; i++)
+          actorsArray.push(risultati[i].name);
+        } else {
+          for(var i = 0; i <= 4; i++) {
+            actorsArray.push(risultati[i].name);
+          }
+        }
+        printActors(id, actorsArray);
+      }
+    },
+    "error": function(err) {
+      alert("errore!");
+    }
+  });
+}
+
+// Funzione che stampa i nomi degli attori in pagina
+function printActors(id, actorsArray) {
+  var joinedActors = actorsArray.join(', ');
+  $('[data-id='+id+']').find('.attori').text("Attori: " + joinedActors);
+}
 
 
 
